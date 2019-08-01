e3e0eb2f4214acbb641b1a7df43c158655e3de57
Afzal Najam <afzalnaj@gmail.com>
2019-07-31 23:08:49 -0400

    Improved ViewModelFactory

--- app/src/main/java/com/spacebitlabs/onelock/ViewModelFactory.kt
+++ app/src/main/java/com/spacebitlabs/onelock/ViewModelFactory.kt
@@ -11,20 +11,18 @@ class ViewModelFactory : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val parameterTypes = modelClass.constructors[0].parameterTypes
-        val parameters = mutableListOf<Any>()
-
         val injection = Injection.get()
-
-        for (type in parameterTypes) {
-            val instanceOfType = when(type) {
+        val parameters = Array<Any>(parameterTypes.size, init = {
+            val instanceOfType = when(parameterTypes[it]) {
                 DataStore::class.java -> injection.provideDataStore()
                 else -> throw NoSuchElementException("Injection does not have this type, or not declared in ViewModelFactory")
             }
 
-            parameters.add(instanceOfType)
-        }
+            return@Array instanceOfType
+        })
 
+        // TODO see if there's better Kotlin code than this for this factory
         @Suppress("UNCHECKED_CAST")
-        return modelClass.constructors[0].newInstance(*parameters.toTypedArray()) as T
+        return modelClass.constructors[0].newInstance(*parameters) as T
     }
 }
